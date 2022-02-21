d4096dfda7d2bc5acda4b079a186173d67ad3bcb
Aming <328682979@qq.com>
2022-02-21 21:13:11 +0800

    修改users路由

--- routes/users.js
+++ routes/users.js
@@ -27,7 +27,7 @@ router.get('/about/:id', function(req, res, next) {
   res.send('ID: ' + req.params.id);
 });
 router.use(function(req, res, next) {
-  if(req.originalUrl=== '/users/0') return next('router');
+  if(req.originalUrl=== '/users/01') return next('router');
   next();
 });
 
